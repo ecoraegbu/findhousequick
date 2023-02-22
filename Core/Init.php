359898c86f0a725d1c525d7152de231e771e2105
Emmanuel Oraegbu <ecoraegbu@gmail.com>
2023-02-22 19:29:49 +0100

    added the cookies, congig and core/init.php

---
+++ Core/Init.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+$GLOBALS['config'] = array(
+    'remember' => array(
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 604800
+    ),
+    'session' => array(
+        'session_name' => 'user',
+        'token_name' => 'token'
+    )
+);
+
+spl_autoload_register(function($class){
+    require_once 'Classes/'.$class.'.php';
+});
+require_once 'functions/sanitize.php';
+
+if(Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(config::get('session/session_name'))){
+    $hash = cookie::get(config::get('remember/cookie_name'));
+    $hashcheck = Database::getinstance()->get('userssessions', array('hash', '=', $hash));
+     
+    if($hashcheck->count()){
+        $user = new User($hashcheck->first()->userid);
+        $user->login();
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # findhousequick
 
 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
+
+<meta name="description" content="Find your dream home with FindHouseQuick. Our experienced real estate agents can help you buy or rent a property in any location, whether you're looking for a house, apartment, condo, or commercial property. Explore our listings and search for properties based on your preferred location, price range, and other criteria. Contact us today to start your real estate journey!">
