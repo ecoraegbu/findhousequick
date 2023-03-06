eda073d9f7bd15bec53a179b6e244837245cb88b
mykaenobs <mykaenobs@gmail.com>
2023-03-06 14:59:50 +0100

    Move the login, register and forgot password to root level

--- Core/Init.php
+++ Core/Init.php
@@ -1,16 +1,15 @@
 <?php
 session_start();
-spl_autoload_register(function($class){
-    require_once 'Classes/'.$class.'.php';
-    
+spl_autoload_register(function ($class) {
+    require_once 'Classes/' . $class . '.php';
 });
-require_once 'functions/sanitize.php';
+require_once 'Functions/Sanitize.php';
 
-if(Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))){
+if (Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))) {
     $hash = Cookie::get(Config::get('remember/cookie_name'));
     $hashcheck = Database::getinstance()->get('userssessions', array('hash', '=', $hash));
-     
-    if($hashcheck->count()){
+
+    if ($hashcheck->count()) {
         $user = new User($hashcheck->first()->userid);
         $user->login();
     }
--- login.php
+++ login.php
@@ -1,124 +1,123 @@
 <?php
-require_once 'core/init.php';
+require_once 'Core/Init.php';
 
-Config::get('remember/cookie_name') ;
-if (Input::exists()){
-    
-    if(Token::check(Input::get('token'))){
-        
-        $data =[];
-        $email = Input::get('email');
-        $password = Input::get('password');
-        $data['email'] = $email;
-        $data['password'] = $password;
-        $rules = [
-            'email' => 'required|email|max:255',
-            'password' => 'required|string|min:8|max:255',];
+Config::get('remember/cookie_name');
+if (Input::exists()) {
 
-        $validate = new Validator($data);
-        $result = $validate->validate($rules);
-        if ($validate->passes()){
-            
-            $user = new User();
-            
-            $remember = (Input::get('remember') === 'on') ? true : false;
-        
-           $login = $user->login( Input::get('email'), Input::get('password'), $remember);
-           
-            if($login){
-              
-                Redirect::to('index.php');
-            }  else{
-                die('sorry');
-            }
-        
-        } else {
-                foreach($validate->errors() as $error){
-                  
-                }
-            }
+  if (Token::check(Input::get('token'))) {
+
+    $data = [];
+    $email = Input::get('email');
+    $password = Input::get('password');
+    $data['email'] = $email;
+    $data['password'] = $password;
+    $rules = [
+      'email' => 'required|email|max:255',
+      'password' => 'required|string|min:8|max:255',
+    ];
+
+    $validate = new Validator($data);
+    $result = $validate->validate($rules);
+    if ($validate->passes()) {
+
+      $user = new User();
+
+      $remember = (Input::get('remember') === 'on') ? true : false;
+
+      $login = $user->login(Input::get('email'), Input::get('password'), $remember);
+
+      if ($login) {
+
+        Redirect::to('index.php');
+      } else {
+        die('sorry');
+      }
+    } else {
+      foreach ($validate->errors() as $error) {
+      }
     }
+  }
 }
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Login Form</title>
-    <style>
-      body {
-        font-family: Arial, sans-serif;
-        background-color: #f2f2f2;
-      }
-      .login {
-        width: 400px;
-        margin: 100px auto;
-        background-color: #fff;
-        padding: 20px;
-        border-radius: 10px;
-        box-shadow: 0 0 10px rgba(0,0,0,0.3);
-      }
-      .login h1 {
-        text-align: center;
-        margin-bottom: 20px;
-      }
-      .form-group {
-        margin-bottom: 20px;
-      }
-      label {
-        display: block;
-        margin-bottom: 5px;
-        font-weight: bold;
-      }
-      input[type="text"],
-      input[type="password"] {
-        display: block;
-        width: 100%;
-        padding: 10px;
-        border-radius: 5px;
-        border: 1px solid #ccc;
-        box-sizing: border-box;
-        font-size: 16px;
-        margin-bottom: 10px;
-      }
-      input[type="submit"] {
-        background-color: #007bff;
-        color: #fff;
-        border: none;
-        padding: 10px;
-        border-radius: 5px;
-        font-size: 16px;
-        cursor: pointer;
-      }
-      input[type="submit"]:hover {
-        background-color: #0062cc;
-      }
-      .error {
-        color: #dc3545;
-        margin-top: 10px;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="login">
-      <h1>Login</h1>
-      <form action="login.php" method="post">
-        <div class="form-group">
-          <label for="email">Username</label>
-          <input type="text" name="email" id="email" >
+<?php include('./Templates/Auth/Header.php') ?>
+
+<!-- Page Title -->
+<title>Sign In | FindHouseQuick</title>
+</head>
+
+<body>
+  <div class="grid grid-cols-1 md:grid-cols-none h-screen">
+    <section class="bg-login hover:grayscale grayscale-0 transition-all hidden md:block bg-blend-multiply bg-primary bg-opacity-50 col-start-1 col-end-10">
+      <h1 class="text-white text-2xl font-bold py-6 px-12">FindHouseQuick</h1>
+      <p></p>
+    </section>
+    <section class="md:col-start-10 md:col-end-12 grid place-items-center">
+
+      <div class="w-full px-6 max-w-sm">
+        <h1 class="block md:hidden  text-white font-bold mb-10 bg-primary p-2 text-center rounded-lg text-2xl">FindHouseQuick</h1>
+
+        <h2 class="text-3xl font-bold text-gray-900">Welcome back!</h2>
+        <p class="text-sm text-gray-500 mt-1">Start managing your properties better and easy</p>
+
+        <div class="mt-4 bg-error p-4 rounded-lg text-white flex justify-between items-center gap-4">
+          <div class="">
+            <p class="font-bold -mb-1">Unable to login</p>
+            <p class="text-sm">User details found, please try again</p>
+          </div>
+          <i icon-name="x" class="h-4 w-4"></i>
         </div>
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" name="password" id="password" >
+
+        <form action="" class="mt-10">
+
+          <div class="relative bg-main flex items-center pl-2 rounded-lg">
+            <span class="inline-block bg-white p-2 text-primary rounded-lg">
+              <i icon-name="mail" class="h-4 w-4"></i>
+            </span>
+            <input type="text" name="email" autocomplete="nope" placeholder="you@example.com" class="text-sm px-2 py-4 bg-main text-gray-700 rounded-lg w-full outline-none">
+          </div>
+          <!-- <small class="text-red-500">Email field is required</small> -->
+
+          <div class="relative bg-main flex items-center pl-2 rounded-lg mt-3">
+            <span class="inline-block bg-white p-2 text-primary rounded-lg">
+              <i icon-name="lock" class="h-4 w-4"></i>
+            </span>
+            <input type="password" name="password" autocomplete="nope" placeholder="Your Password" class="text-sm px-2 py-4 bg-main text-gray-700 rounded-lg w-full outline-none">
+          </div>
+          <small class="text-red-500">Password field is required</small>
+
+          <?php if (isset($error)) : ?>
+            <small class="text-red-500"><?php echo $error; ?></small>
+          <?php endif; ?>
+
+
+          <div class="text-right mt-1">
+            <a href="forgot.php" class="text-sm text-primary font-semibold">Forgot Password?</a>
+          </div>
+
+          <!-- Token generation -->
+          <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+
+          <button type="submit" class="mt-6 w-full px-6 py-3 text-white text-sm rounded-lg bg-primary font-medium hover:bg-blue-600">Sign In</button>
+
+        </form>
+
+
+        <div class="flex gap-2 items-center mt-8 mb-4">
+          <span class="bg-gray-100 h-0.5 w-full"></span>
+          <span class="flex-shrink-0 text-sm w-8 h-8 rounded-full grid place-items-center text-gray-600">
+            Or
+          </span>
+          <span class="bg-gray-100 h-0.5 w-full"></span>
         </div>
-        <div class="form-group">
-            <input type ="hidden" name="token" value ="<?php echo Token::generate(); ?>">
-          <input type="submit" value="Login">
+
+
+        <div class="text-center">
+          <a href="register.php" class="text-gray-600 text-sm">Don't have an account? <span class="text-primary font-semibold">Sign Up</span></a>
         </div>
-        <?php if (isset($error)): ?>
-        <div class="error"><?php echo $error; ?></div>
-        <?php endif; ?>
-      </form>
-    </div>
-  </body>
-</html>
+
+      </div>
+
+    </section>
+  </div>
+  <?php include('./Templates/Auth/Footer.php') ?>
+</body>
