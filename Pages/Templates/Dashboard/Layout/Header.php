587949557d7b57f04bbb8ffb851c6bff94f290d9
mykaenobs <mykaenobs@gmail.com>
2023-03-09 12:32:40 +0100

    Updated the dropdown functionality and add the dropdown to the profile icon on the header section for the navbar

--- Pages/Templates/Dashboard/Layout/Header.php
+++ Pages/Templates/Dashboard/Layout/Header.php
@@ -22,12 +22,32 @@
       </ul>
     </div>
 
-    <div class="flex items-center gap-3 text-sm">
-      <img src="./../Assets/login.jpg" class="object-cover h-8 w-8 bg-primary rounded-full" alt="">
-      <div class="">
-        <p class="font-semibold -mb-1">Sarah Reichert</p>
-        <small class="text-gray-400">Estate developer</small>
+    <div class="relative" x-data="{ open: false }">
+      <div class="flex items-center gap-3 text-sm">
+        <img src="./../Assets/login.jpg" class="object-cover h-8 w-8 bg-primary rounded-full" alt="">
+        <div class="cursor-pointer" @click="open = !open">
+          <p class="font-semibold -mb-1">Sarah Reichert</p>
+          <small class="text-gray-400">Estate developer</small>
+        </div>
+
+
+        <ul class="absolute top-full mt-2 right-0 py-2 rounded-lg bg-white shadow-sm w-52" x-cloak x-show="open" @click.outside="open = false">
+          <li class="">
+            <a href="#" class="flex gap-2 px-4 py-3 border-b border-gray-100 text-gray-400 hover:text-gray-700">
+              <i icon-name="user" class="h-4 w-4"></i>
+              <span>Profile</span>
+            </a>
+          </li>
+
+          <li class="">
+            <a href="#" class="flex gap-2 px-4 py-3 text-gray-400 hover:text-gray-700">
+              <i icon-name="log-out" class="h-4 w-4"></i>
+              <span>Logout</span>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
+
   </div>
 </header>
