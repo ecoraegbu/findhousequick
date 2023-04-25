e01609bcded5be73d5ba8026a63084f8464e2376
Mykaenobs <mykaenobs@gmail.com>
2023-04-25 11:43:11 +0100

    done for the landing page redesign

--- Pages/Home.php
+++ Pages/Home.php
@@ -20,10 +20,10 @@ $properties = $property->get_all_property();
   <section class="">
     <div class="bg-primary md:bg-gradient-to-r md:from-primary md:via-primary md:to-primary-light overflow-hidden h-auto bg-cover">
       <div class="relative h-full flex items-end justify-between">
-        <div class="px-10 py-14 md:px-20 flex items-center h-full">
+        <div class="px-10 py-8 md:px-11 flex items-center h-full">
           <div class="relative z-10">
-            <h1 class="text-6xl font-bold text-white ">
-              Let's Find Your <br> Dream Home. </h1>
+            <h1 class="text-5xl font-bold text-white ">
+              Let's Find Your Dream Home. </h1>
             <p class="max-w-lg mt-4 mb-8 text-white ">
               We are a real estate agency that will help you find the best residence you dream of, let's
               discuss for your dream house!
@@ -52,7 +52,7 @@ $properties = $property->get_all_property();
 
 
   <!-- Listings Section -->
-  <section class="px-10 sm:px-20 md:px-10 my-8">
+  <section class="px-10 sm:px-20 md:px-10 my-5">
     <div class="max-w-7xl mx-auto">
       <!-- <span class="text-primary font-light text-lg">Our featured properties.</span>
       <h1 class="text-5xl font-bold text-text before:block before:bg-gray-100 before:h-48 before:w-36 before:absolute before:left-0 before:-mt-16 before:rounded-lg before:-z-10">Our Listings.</h1>
@@ -61,7 +61,7 @@ $properties = $property->get_all_property();
       <div class="flex gap-12">
 
         <div class="basis-60 hidden lg:block">
-          <div class="sticky top-0 z-10  py-4 shadow-lg">
+          <div class="sticky top-0 z-10 py-4 shadow-md bg-gray-50">
             <p class="text-xl text-slate-700 font-medium px-4">Categories</p>
             <ul>
               <li class="group px-4">
@@ -77,7 +77,7 @@ $properties = $property->get_all_property();
                   </div>
                 </div>
 
-                <div class="absolute hidden shadow-lg bg-white group-hover:block top-0 left-full h-64 w-60">
+                <div class="absolute hidden shadow-lg bg-white group-hover:block top-0 left-full h-auto w-60">
                   <div class="group-hover px-4 py-2">
                     <p class="text-slate-400 -mb-1">Cars</p>
                     <span class="text-slate-600">24,000 ads</span>
@@ -96,6 +96,43 @@ $properties = $property->get_all_property();
                   </div>
                 </div>
               </li>
+
+              <li class="group px-4">
+                <div class="border-b group-hover border-b-gray-200 py-2 flex justify-between items-center">
+                  <div class="">
+
+                    <p class="text-slate-400 -mb-1">Mansions</p>
+                    <span class="text-slate-600">24,000 ads</span>
+
+                  </div>
+                  <div class="">
+                    <i icon-name="chevron-right" class="h-4 w-4"></i>
+                  </div>
+                </div>
+
+                <div class="absolute hidden shadow-lg bg-white group-hover:block top-0 left-full h-auto w-60">
+                  <div class="group-hover px-4 py-2">
+                    <p class="text-slate-400 -mb-1">Mansions 1</p>
+                    <span class="text-slate-600">24,000 ads</span>
+                  </div>
+                  <div class="group-hover px-4 py-2">
+                    <p class="text-slate-400 -mb-1">Mansions 2</p>
+                    <span class="text-slate-600">24,000 ads</span>
+                  </div>
+                  <div class="group-hover px-4 py-2">
+                    <p class="text-slate-400 -mb-1">Mansions 3</p>
+                    <span class="text-slate-600">24,000 ads</span>
+                  </div>
+                  <div class="group-hover px-4 py-2">
+                    <p class="text-slate-400 -mb-1">Mansions 4</p>
+                    <span class="text-slate-600">24,000 ads</span>
+                  </div>
+                  <div class="group-hover px-4 py-2">
+                    <p class="text-slate-400 -mb-1">Mansions 5</p>
+                    <span class="text-slate-600">24,000 ads</span>
+                  </div>
+                </div>
+              </li>
             </ul>
           </div>
         </div>
@@ -103,18 +140,16 @@ $properties = $property->get_all_property();
 
         <div class="flex-1">
 
-
           <div class="splide" data-splide='{"type":"loop"}' aria-label="Splide Basic HTML Example">
             <div class="splide__track">
               <ul class="splide__list">
-                <li class="splide__slide h-72 w-full">
+                <li class="splide__slide h-60 w-full">
                   <img src="https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" class="w-full h-full object-cover">
                 </li>
-                <li class="splide__slide h-72 w-full">
+                <li class="splide__slide h-60 w-full">
                   <img src="https://images.pexels.com/photos/7193708/pexels-photo-7193708.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" class="w-full h-full object-cover">
-
                 </li>
-                <li class="splide__slide h-72 w-full">
+                <li class="splide__slide h-60 w-full">
                   <img src="https://images.pexels.com/photos/16082425/pexels-photo-16082425.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" class="w-full h-full object-cover">
 
                 </li>
@@ -123,7 +158,7 @@ $properties = $property->get_all_property();
           </div>
 
 
-          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mt-10">
+          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 mt-10">
             <?php
             foreach ($properties as $property) : ?>
               <?php $urls = json_decode($property->images);
@@ -152,8 +187,8 @@ $properties = $property->get_all_property();
                   <img src="<?= $relative_path ?>" class="aspect-video object-cover w-full rounded-lg" alt="">
                   <div class="w-full h-full bg-black bg-opacity-50 opacity-0  group-hover:opacity-100 absolute inset-0 rounded-lg p-2 transition-all">
                     <div class="flex flex-wrap gap-1">
-                      <span class="bg-primary text-white px-2 py-1.5 text-sm rounded-md"><?= ucfirst($property->status) ?></span>
-                      <span class="bg-success text-white px-2 py-1.5 text-sm rounded-md"><?= ucfirst($property->purpose) ?></span>
+                      <span class="bg-primary text-white px-2 py-1.5 text-xs rounded-md"><?= ucfirst($property->status) ?></span>
+                      <span class="bg-success text-white px-2 py-1.5 text-xs rounded-md"><?= ucfirst($property->purpose) ?></span>
                       <!-- <span class="bg-purple-700 text-white px-2 py-1.5 text-sm rounded-md">For Rent</span>
                   <span class="bg-error text-white px-2 py-1.5 text-sm rounded-md">Occupied</span> -->
                     </div>
